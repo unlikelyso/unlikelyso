a1362c0eec000d3da32be9e0c6d41e3948f1e83a
unlikelyso <136288121+unlikelyso@users.noreply.github.com>
2024-04-10 02:06:06 -0700

    Updating copy

--- src/js/index.js
+++ src/js/index.js
@@ -43,15 +43,16 @@ function navHighlighter() {
   let scrollY = window.pageYOffset;
 
   // Now we loop through sections to get height, top and ID values for each
+    /*
   sections.forEach((current) => {
     const sectionHeight = current.offsetHeight;
     const sectionTop = current.offsetTop - 50;
     sectionId = current.getAttribute("id");
 
-    /*
     - If our current scroll position enters the space where current section on screen is, add .active class to corresponding navigation link, else remove it
     - To know which link needs an active class, we use sectionId variable we are getting while looping through sections as an selector
     */
+    /*
     if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
       document
         .querySelector(".ts-navbar a[href*=" + sectionId + "]")
@@ -62,4 +63,5 @@ function navHighlighter() {
         .classList.remove("ts-text-blue-2");
     }
   });
+    */
 }
